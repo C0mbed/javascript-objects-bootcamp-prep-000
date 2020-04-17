c419e4ef64abde78388767cf8c67d94240a5cb71
Matt Farncombe <matt@farncombe.co>
2020-04-17 01:25:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {U2: 'Beautiful Day'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  console.log('playlist is ' + playlist);
   return Object.assign(playlist, artistName, songTitle);
 }
