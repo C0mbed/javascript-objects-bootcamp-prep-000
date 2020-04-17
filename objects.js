cce2b1a03577fd16ebdc7478098656f705a34904
Matt Farncombe <matt@farncombe.co>
2020-04-17 01:13:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
-var playlist = {artist1:'U2', song_title1: 'Beautiful Day'};
+var playlist = {[{artist1:'U2', song_title1: 'Beautiful Day'}]};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   var playListNumber = playlist.length;
+
   return Object.assign(playlist, 'artist' + playListNumber, artistName, 'song_title' + playListNumber, songTitle)
 }
